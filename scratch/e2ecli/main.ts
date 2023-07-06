e15a4a26898b195504fc29e1e2e856aad9cca819
Julian Scharf <julzelements@gmail.com>
2023-07-06 15:53:53 +1000

    cli is e2e tested

--- scratch/e2ecli/main.ts
+++ scratch/e2ecli/main.ts
@@ -5,9 +5,19 @@ const readline = createInterface({
   output: process.stdout,
 });
 
-readline.question("What is your name?", (name: string) => {
-  readline.question("What is your age?", (age: string) => {
-    console.log(`Hello ${name}, you are ${age} years old.`);
-    readline.close();
+function question(query: string): Promise<string> {
+  return new Promise((resolve) => {
+    readline.question(query, (answer) => {
+      resolve(answer);
+    });
   });
-});
+}
+
+async function main() {
+  const name = await question("What is your name? ");
+  const age = await question("What is your age? ");
+  console.log(`Hello ${name}, you are ${age} years old.`);
+  readline.close();
+}
+
+main();
